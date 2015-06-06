4eb9ab14c7be944ab226d11c530c9f3929218c54
Takafumi ONAKA <takafumi.onaka@gmail.com>
2015-06-07 00:11:13 +0900

    add Global config

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,11 @@ Bundler.require(*Rails.groups)
 
 module Sharedoc
   class Application < Rails::Application
+    Global.configure do |config|
+      config.environment = Rails.env.to_s
+      config.config_directory = Rails.root.join("config/global").to_s
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
