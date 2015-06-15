e58ef3c8e3393c6459af5efb2940f63b9cb7b14c
Takafumi ONAKA <takafumi.onaka@gmail.com>
2015-06-16 01:59:16 +0900

    use kuromoji index

--- app/models/presentation_outline.rb
+++ app/models/presentation_outline.rb
@@ -17,4 +17,30 @@ class PresentationOutline < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
   belongs_to :presentation
+
+  settings index: {
+    analysis: {
+      filter: {
+        pos_filter: {
+          type: "kuromoji_part_of_speech",
+          stopgaps: ["助詞-格助詞-一般", "助詞-終助詞"]
+        },
+        greek_lowercase_filter: {
+          type: "lowercase",
+          language: "greek"
+        }
+      },
+      analyzer: {
+        kuromoji_analyzer: {
+          type: "custom",
+          tokenizer: "kuromoji_tokenizer",
+          mode: "extended",
+          filter: %w(kuromoji_baseform pos_filter greek_lowercase_filter cjk_width)
+        }
+      }
+    }
+  }
+  mapping do
+    indexes :body, analyzer: :kuromoji_analyzer
+  end
 end
