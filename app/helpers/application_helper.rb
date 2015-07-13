549e9b2706cdb052f01d6943167b89e8d7098d2b
Takafumi ONAKA <takafumi.onaka@gmail.com>
2015-07-13 23:12:42 +0900

    helper に追い出す

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def name_with_avatar(user)
     image_tag(user.image_url, size: "24x24") + link_to(user.name, user_path(username: user.username))
   end
+
+  def presentation_image_tag(presentation)
+    # TODO: 変換中を表現するメソッドを model に用意
+    if presentation.image_file?
+      image_tag(presentation.image_file_url, class: "blockimage")
+    else
+      image_tag("converting.png", size: "300x225", alt: "変換中", class: "blockimage")
+    end
+  end
 end
