ce3927823221809238248aa17ab367d95b487f99
Takafumi ONAKA <takafumi.onaka@gmail.com>
2015-07-17 23:04:17 +0900

    add TODO for performance issue

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -13,6 +13,7 @@ class PresentationsController < ApplicationController
   def search
     search_param = { query: { match: { body: { query: params[:q], operator: "and" } } } }
     presentation_ids = PresentationOutline.search(search_param).map(&:presentation_id)
+    # TODO: id の時点で paginate できるはずなので ordered_find_by_id にかける前に処理したい
     @presentations = Presentation.is_public.ordered_find_by_id(presentation_ids).compact.paginate(page: params[:page])
     render "index"
   end
