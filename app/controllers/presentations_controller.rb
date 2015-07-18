73ffae88802da7976f0edfc55aba226b764a2dfd
Takafumi ONAKA <takafumi.onaka@gmail.com>
2015-07-19 01:13:11 +0900

    layout ファイルだけで全てが終わってしまったので show に移動

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -11,7 +11,7 @@ class PresentationsController < ApplicationController
   def show
     @user = User.find_by!(username: params[:username])
     @presentation = @user.presentations.is_public.find(params[:id])
-    render layout: "viewer"
+    render layout: nil
   end
 
   def search
