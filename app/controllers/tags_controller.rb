a3b5eec0a183add1f2827b1fa156fbfe177fcb93
Takafumi ONAKA <takafumi.onaka@gmail.com>
2015-07-27 17:40:51 +0900

    tag 検索の表示順を published_at desc に修正

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @presentations = Presentation.tagged_with(params[:name]).paginate(page: params[:page])
+    @presentations = Presentation.tagged_with(params[:name]).order(published_at: :desc).paginate(page: params[:page])
     render "presentations/index"
   end
 end
