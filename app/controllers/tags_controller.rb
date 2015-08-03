10ee3168d80e60f583edfb136f820cea5bf81c34
Takafumi ONAKA <takafumi.onaka@gmail.com>
2015-08-03 14:45:12 +0900

    タグ検索時に is_public フラグが効いていなかった

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @presentations = Presentation.tagged_with(params[:name]).order(published_at: :desc).paginate(page: params[:page])
+    @presentations = Presentation.tagged_with(params[:name]).is_public.order(published_at: :desc).paginate(page: params[:page])
     render "presentations/index"
   end
 end
