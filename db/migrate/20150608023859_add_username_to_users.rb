a9bdd6ade3b1a13cec4b72b033c3a0423bb58508
Takafumi ONAKA <takafumi.onaka@gmail.com>
2017-10-22 17:11:18 +0900

    Fix boolean symbol :true => true

--- db/migrate/20150608023859_add_username_to_users.rb
+++ db/migrate/20150608023859_add_username_to_users.rb
@@ -1,6 +1,6 @@
 class AddUsernameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :username, :string, null: false, after: :name
-    add_index :users, :username, unique: :true
+    add_index :users, :username, unique: true
   end
 end
