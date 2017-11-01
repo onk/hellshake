3fec77b3e3297bea1c469c862360183fe968a0bb
Daisuke Hayata <daisuke.hayata@happyelements.co.jp>
2017-11-01 19:32:05 +0900

    Update auth settings for google login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,6 +6,7 @@ Devise.setup do |config|
   end
   config.omniauth :facebook, Global.omniauth.facebook.key, Global.omniauth.facebook.secret
   config.omniauth :twitter,  Global.omniauth.twitter.key,  Global.omniauth.twitter.secret
+  config.omniauth :google_oauth2,  Global.omniauth.google.key,  Global.omniauth.google.secret, {prompt:"select_account",  hd: 'example.co.jp', name: :google}
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
